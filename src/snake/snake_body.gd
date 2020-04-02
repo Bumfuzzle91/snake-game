a68cfe891dad6e1af71c216abc6b712c9a1777ee
Bumfuzzle91 <bumfuzzle91@gmail.com>
2020-04-02 00:28:20 -0500

    Made the snakes path a child of its body polygon node. the path is now more of a utility class, and has to be used and updated in the body script. The path is now also its own scene making editing the path by accident much more difficult.

--- src/snake/snake_body.gd
+++ src/snake/snake_body.gd
@@ -2,15 +2,18 @@
 extends Polygon2D
 
 export(int) var width = 64
+export(int) var length = 512
+export(int) var resolution = 60
+
 export(Texture) var scale_texture
 export(CurveTexture) var distort_curve_texture
-
-export(NodePath) var path2d_path
 
 var path
 var follow
 var curve
 var distort_curve
+
+onready var head = get_parent()
 
 var rid
 var tex
@@ -19,7 +22,7 @@ var verts
 var uvs
 
 func _ready():
-	path = get_node(path2d_path)
+	path = $SnakePath
 	follow = path.get_child(0)
 	curve = path.curve
 	distort_curve = distort_curve_texture.curve
@@ -28,10 +31,13 @@ func _ready():
 	rid = get_canvas_item()
 	tex = scale_texture.get_rid()
 	
+	path.generate_path(length, resolution)
+	
 	create_mesh_from_path()
 	update()
 
 func _process(delta):
+	path.follow_target(head)
 	create_mesh_from_path()
 	update()
 
@@ -52,44 +58,44 @@ func _draw():
 		count += 2
 
 	indices = PoolIntArray(indices)
-	
+
 	#move the transform to align with the head
 	draw_set_transform(Vector2(-to_global(position)), rotation, scale)
-	
+
 	VisualServer.canvas_item_add_triangle_array(rid, indices, verts, PoolColorArray(), uvs,
 			PoolIntArray(), PoolRealArray(), tex)
 
 func create_mesh_from_path():
 	verts = []
 	uvs = []
-	
+
 	var points = curve.get_baked_points()
 	var segments = points.size()
-	
+
 	var point_offset = Vector2(width, width) * .5
 	follow.v_offset = width
-	
+
 	#reverse the curve so that it is drawn (from our perspective) back to front
 	#otherwise the snake would appear to go under itself when overlapping itself
 	points.invert()
-	
+
 	for i in range(0, segments):
 		var point = points[i]
 		var follow_offset = curve.get_closest_offset(point)
 		var normal = (follow.position - point).normalized()
-		
+
 		#path follow offset in pixels
 		follow.offset = follow_offset
-		
+
 		#multiply offset by distortion curve. values for interpolate expected to be normalized
 		var new_point_offset = point_offset * distort_curve.interpolate(follow_offset / curve.get_baked_length())
-		
+
 		verts.append(point - new_point_offset * normal)
 		verts.append(point + new_point_offset * normal)
-		
+
 		uvs.append(Vector2(0, i / (segments - 1.0)))
 		uvs.append(Vector2(1, i / (segments - 1.0)))
-		
+
 	verts = PoolVector2Array(verts)
 	uvs = PoolVector2Array(uvs)
-	
+
--- src/snake/snake_path.gd
+++ src/snake/snake_path.gd
@@ -1,25 +1,25 @@
 #snake_path.gd
 extends Path2D
 
-export(NodePath) var head_path
-export var length = 512
-export var resolution = 60
+#onready var follow = $PathFollow2D
 
-var head
-onready var follow = $PathFollow2D
+var length = 0
+var resolution = 0
 
-func _ready():
-	head = get_node(head_path)
+func generate_path(_length, _resolution):
 	curve.clear_points()
-
+	
+	length = _length
+	resolution = _resolution
+	
 	#total number of points = resolution + 1
 	for i in range(0, resolution + 1):
 		var pos = Vector2(i * length/resolution, 0)
 		
 		curve.add_point(pos)
-
-func _process(delta):
-	curve.set_point_position(0, to_global(head.position))
+	
+func follow_target(target):
+	curve.set_point_position(0, to_global(target.position))
 	for i in range(0, curve.get_point_count()-1):
 		var p1 = curve.get_point_position(i)
 		var p2 = curve.get_point_position(i+1)
@@ -27,8 +27,8 @@ func _process(delta):
 		var vec = p1 - p2
 		vec = vec.normalized() * length / resolution	
 		curve.set_point_position(i+1, p1 - vec)
-	
+
 func _draw():
+#	draw_set_transform(Vector2(-to_global(position)), rotation, scale)
+#	draw_polyline(curve.get_baked_points(), Color.green, 20, true)
 	pass
-	#draw_set_transform(Vector2(-to_global(position)), rotation, scale)
-	#draw_polyline(curve.get_baked_points(), Color.green, 20, true)
