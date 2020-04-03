994ec453f8eaa0b6460955f673c1de0a3340b95e
Bumfuzzle91 <bumfuzzle91@gmail.com>
2020-04-02 23:33:59 -0500

    progress commit for giving body a seperate distort method

--- src/snake/snake_pseudo_mesh.gd
+++ src/snake/snake_pseudo_mesh.gd
@@ -12,6 +12,8 @@ var tex
 
 var verts
 var uvs
+var path_normals
+var path_offsets
 
 func _ready():
 	body = get_parent()
@@ -23,10 +25,11 @@ func _ready():
 	rid = body.get_canvas_item()
 	tex = body.scale_texture.get_rid()
 	
-	create_mesh_from_path(path, body.width)
+	assign_mesh_data_from_path(path, body.width)
+	update()
 	
 func _process(delta):
-	create_mesh_from_path(path, body.width)
+	assign_mesh_data_from_path(path, body.width)
 	update()
 	
 func _draw():
@@ -45,7 +48,7 @@ func _draw():
 
 		count += 2
 
-	indices = PoolIntArray(indices)
+	#indices = PoolIntArray(indices)
 	
 	#regualr canvas item draw commands don't seem to work here
 	#my theory is that because node2d doesn't draw, nothing is done automatically
@@ -59,40 +62,49 @@ func _draw():
 #
 	VisualServer.canvas_item_add_triangle_array(rid, indices, verts, PoolColorArray(), uvs,
 			PoolIntArray(), PoolRealArray(), tex)
-	
-func create_mesh_from_path(path, width):
+
+func assign_mesh_data_from_path(path, width):
 	verts = []
 	uvs = []
+	path_normals = []
+	path_offsets = []
 	
 	var points = path.curve.get_baked_points()
 	var segments = points.size()
 
 	var point_offset = Vector2(width, width) * .5
-	follow.v_offset = width
-
+	
 	#reverse the curve so that it is drawn (from our perspective) back to front
 	#otherwise the snake would appear to go under itself when overlapping itself
 	points.invert()
-
+	
+	follow.v_offset = width
 	for i in range(0, segments):
 		var point = points[i]
-		var follow_offset = curve.get_closest_offset(point)
-		
+		var follow_pixel_offset = curve.get_closest_offset(point)
+		var path_offset = follow_pixel_offset / curve.get_baked_length()
 
 		#path follow offset in pixels
 		#must offset before getting normal, otherwise there are artifacts
-		follow.offset = follow_offset
+		follow.offset = follow_pixel_offset
 		var normal = (follow.position - point).normalized()
-		
-		var distort_curve = body.distort_curve_texture.curve
-		#multiply offset by distortion curve. values for interpolate expected to be normalized
-		var new_point_offset = point_offset * distort_curve.interpolate(follow_offset / curve.get_baked_length())
-		
-		verts.append(point - new_point_offset * normal)
-		verts.append(point + new_point_offset * normal)
 
+		verts.append(point + point_offset * -normal)
+		verts.append(point + point_offset * normal)
+		
 		uvs.append(Vector2(0, i / (segments - 1.0)))
 		uvs.append(Vector2(1, i / (segments - 1.0)))
+		
+		path_normals.append(-normal)
+		path_normals.append(normal)
+		
+		path_offsets.append(follow.offset)
+		path_offsets.append(follow.offset)
+		
+func distort_mesh():
+	pass
+#		var distort_curve = body.distort_curve_texture.curve
+#
+#		#multiply offset by distortion curve. values for interpolate expected to be normalized
+#		var new_point_offset = point_offset * distort_curve.interpolate(path_offset)
 
-	verts = PoolVector2Array(verts)
-	uvs = PoolVector2Array(uvs)
